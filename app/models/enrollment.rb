2a729fae959c6ca58222ba2a6c86333a13857af8
Logan Hartman <loganchart@gmail.com>
2022-12-08 16:04:40 -0500

    associations

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,10 +1,20 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
-    has_many :students,
+    belongs_to :students,
         primary_key: :id,
         foreign_key: :student_id,
         class_name: :User
    
-    has_many :classes,
+    belongs_to :classes,
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Course
